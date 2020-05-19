709036661ca63d30d789c2004bc5a63ae0f26533
deepashrangan7 <deepashrangan7>
2020-05-19 13:44:17 +0530

    writting test cases

---
+++ src/com/tests/LeapYearTest.java
@@ -0,0 +1,36 @@
+package com.tests;
+
+import org.junit.Test;
+
+public class LeapYearTest {
+
+	@Test
+	public void testLeapYearDivideBy400() {
+		assert(false);
+	}
+	@Test
+	public void testLeapYearNotDivideBy400() {
+		assert(false);
+	}
+	@Test
+	public void testLeapYearDividyBy100NotBy400() {
+		assert(false);
+	}
+	@Test
+	public void testLeapYearDividyBy100AndBy400() {
+		assert(false);
+	}
+	@Test
+	public void testLeapYearDividyBy4AndNotBy100() {
+		assert(false);
+	}
+	@Test
+	public void testLeapYearDividyBy4AndBy100() {
+		assert(false);
+	}
+	@Test
+	public void testLeapYearNotDivideBy4() {
+		assert(false);
+	}
+	
+}
